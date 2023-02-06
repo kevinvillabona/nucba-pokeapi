9f1503bfc6400ec01ba80e8a0544cd5a4fcf57b9
Kevin <kevinvillabona@gmail.com>
2023-02-06 20:04:04 -0300

    Implem. buscador y mejora en diseno simil. desafio 1.20

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,9 @@
-const cardsContainer = document.getElementById("cards");
+const container = document.getElementById("container");
+const form = document.getElementById("form");
+const input = document.querySelector(".input");
 const loader = document.querySelector(".pokeball");
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const cargarBtn = document.getElementById("cargar");
 
 // se define isFetching para activarla cuando estemos haciendo una llamada. Y un objeto que va a almacenar la siguiente llamada a la api, en un principio va a ser null porque todavia no hicimos ninguna llamada
 let isFetching = false;
@@ -11,57 +14,87 @@ const nextURL = {
 
 const renderPokemon = (pokemon) => {
   const { id, name, sprites, height, weight, types } = pokemon;
-  const bgColor = (types.length) > 1 ? 
-  `${types[1].type.name}` 
-  : `${types[0].type.name}` ;
+  const bgColor =
+    types.length > 1 ? `${types[1].type.name}` : `${types[0].type.name}`;
+  const sprite = !sprites.other.home.front_default
+    ? `${sprites.other["official-artwork"].front_default}`
+    : `${sprites.other.home.front_default}`;
   return `
-  <div class="poke" style="background-image: linear-gradient(to bottom, rgba(255,255,255,0), var(--${bgColor})), url(assets/img/bg/Type_Background_${types[0].type.name}.webp)">
-  <p class="id-poke">#${id}</p>
-  <img src="${sprites.other.home.front_default}" alt="${name}" />
-          <h2>${name.replace(/\b\w/g, l => l.toUpperCase())}</h2>
-          <span class="exp">EXP. BASE: ${pokemon.base_experience}</span>
-          <div class="poke-card"></div>
-          <div class="poke-info">
-            <div class="info border-r">
-              ${weight / 10}kg
-              <p class="sub">PESO</p>
+    <div class="poke" style="background-image: linear-gradient(to bottom, rgba(255,255,255,0), var(--${bgColor})), url(assets/img/bg/Type_Background_${
+    types[0].type.name
+  }.webp)">
+    <p class="id-poke">#${id}</p>
+    <img src="${sprite}" alt="${name}" />
+            <h2>${name.replace(/\b\w/g, (l) => l.toUpperCase())}</h2>
+            <span class="exp">EXP. BASE: ${pokemon.base_experience}</span>
+            <div class="poke-card"></div>
+            <div class="poke-info">
+              <div class="info border-r">
+                ${weight / 10}kg
+                <p class="sub">PESO</p>
+              </div>
+              ${types
+                .map((tipo) => {
+                  return `<div class="info">
+                  <img src="assets/img/icons/${tipo.type.name}.png" alt="${
+                    tipo.type.name
+                  }-icon" class="tipo-icon" />
+                  <p class="sub">${tipo.type.name.toUpperCase()}</p>
+                </div>`;
+                })
+                .join("")}
+              <div class="info border-l">
+                ${height / 10}m
+                <p class="sub">ALTURA</p>
+              </div>
             </div>
-            ${types
-              .map((tipo) => {
-                return `<div class="info">
-                <img src="assets/img/icons/${tipo.type.name}.png" alt="${tipo.type.name}-icon" class="tipo-icon" />
-                <p class="sub">${tipo.type.name.toUpperCase()}</p>
-              </div>`;
-              })
-              .join("")}
-            <div class="info border-l">
-              ${height / 10}m
-              <p class="sub">ALTURA</p>
-            </div>
-          </div>
-          <span class="border-t"></span>
-          <p class="info-adicional">Región: ${id<152? "Kanto" : id<252 ? "Johto": id<387 ? "Hoenn": id<494 ? "Sinnoh": id<650 ? "Teselia": id<722 ? "Kalos": id<810 ? "Alola": id<906 ? "Galar": "Otras"}</p>
-
-  </div>
-  `;
+            <span class="border-t"></span>
+            <p class="info-adicional">Región: ${
+              id < 152
+                ? "Kanto"
+                : id < 252
+                ? "Johto"
+                : id < 387
+                ? "Hoenn"
+                : id < 494
+                ? "Sinnoh"
+                : id < 650
+                ? "Teselia"
+                : id < 722
+                ? "Kalos"
+                : id < 810
+                ? "Alola"
+                : id < 899
+                ? "Galar"
+                : id < 906
+                ? "Hisui"
+                : "Otra"
+            }</p>
+  
+    </div>
+    `;
 };
 
-
-// renderizar las cards
-const renderPokemonList = (pokeList) => {
-  const cards = pokeList
-    .map((pokemon) => {
-      return renderPokemon(pokemon);
-    })
-    .join("");
-  cardsContainer.innerHTML += cards;
+// se traen los datos del pokemon a buscar
+const fetchPokemonSearched = async (id) => {
+  try {
+    const res = await fetch(`${baseURL + id}`);
+    const data = await res.json();
+    return data;
+  } catch {
+    return null;
+  }
 };
 
-// se traen los datos
-const fetchPokemons = async () => {
-  const res = await fetch(`${baseURL}?limit=8&offset=0`);
-  const data = await res.json();
-  return data;
+// se traen los datos de los pokemon al scrollear
+const fetchPokemonAll = async () => {
+  try {
+    const res = await fetch(`${baseURL}?limit=8&offset=0`);
+    const data = await res.json();
+    return data;
+  } catch {
+    return null;
+  }
 };
 
 const loadAndPrint = (pokemonList) => {
@@ -73,9 +106,70 @@ const loadAndPrint = (pokemonList) => {
   }, 1000);
 };
 
+//distintos casos de muestra del container
+const emptyContainer = () => {
+  // container.innerHTML= `
+  // <h2 style="margin-top: 2rem;">Por favor, ingrese un número.</h2>
+  // `
+  // cargarBtn.classList.add("hidden");
+  location.reload();
+};
+
+const renderResult = async (pokemon) => {
+  const newPk = await pokemon;
+  if (!newPk) {
+    container.innerHTML = `
+        <h2 style="color:red; margin-top: 2rem;">Ningún Pokémon coincide con tu búsqueda.</h2>
+        `;
+  } else {
+    container.innerHTML = renderPokemon(newPk);
+  }
+};
+
+// renderizar las cards
+const renderPokemonList = (pokeList) => {
+  const cards = pokeList
+    .map((pokemon) => {
+      return renderPokemon(pokemon);
+    })
+    .join("");
+  container.innerHTML += cards;
+};
+
+const search = (e) => {
+  cargarBtn.classList.add("hidden");
+  isFetching = true;
+  e.preventDefault();
+  const inputVal = input.value;
+  if (!inputVal) {
+    emptyContainer();
+  }
+  const poke = fetchPokemonSearched(Number(inputVal));
+  renderResult(poke);
+};
+
+const nextPokemons = async () => {
+  isFetching = true;
+  const nextPokemons = await fetch(nextURL.next);
+  const { next, results } = await nextPokemons.json();
+  nextURL.next = next;
+
+  const URLS = results.map((pokemon) => pokemon.url);
+
+  const infoPokemons = await Promise.all(
+    URLS.map(async (url) => {
+      const nextPokemons = await fetch(url);
+      return await nextPokemons.json();
+    })
+  );
+
+  loadAndPrint(infoPokemons);
+};
+
 const init = () => {
+  form.addEventListener("submit", search);
   window.addEventListener("DOMContentLoaded", async () => {
-    let { next, results } = await fetchPokemons();
+    let { next, results } = await fetchPokemonAll();
 
     nextURL.next = next;
 
@@ -90,33 +184,21 @@ const init = () => {
     renderPokemonList(infoPokemones);
   });
 
-  //boton de "cargar mas" que aparezca solo la primera vez para pantallas mayores
-  
   window.addEventListener("scroll", async () => {
+    //por alguna razon, el siguiente codigo no funciona en mobile
     //const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-
     //const bottom = scrollTop + clientHeight >= scrollHeight - 1;
 
-   const m = .1;
-
-   const bottom = m > document.documentElement.scrollHeight - window.scrollY - window.innerHeight
+    const m = 0.1;
+    const bottom =
+      m >
+      document.documentElement.scrollHeight -
+        window.scrollY -
+        window.innerHeight;
 
     if (bottom && !isFetching) {
-      isFetching = true;
-      const nextPokemons = await fetch(nextURL.next);
-      const { next, results } = await nextPokemons.json();
-      nextURL.next = next;
-
-      const URLS = results.map((pokemon) => pokemon.url);
-
-      const infoPokemons = await Promise.all(
-        URLS.map(async (url) => {
-          const nextPokemons = await fetch(url);
-          return await nextPokemons.json();
-        })
-      );
-
-      loadAndPrint(infoPokemons);
+      cargarBtn.classList.add("hidden");
+      nextPokemons();
     }
   });
 };
